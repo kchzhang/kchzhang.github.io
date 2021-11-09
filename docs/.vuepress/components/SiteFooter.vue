9a981507d89e149edb5f8d8c82720cfd9c54ff89
knox.zhang <knox.zhang@wokaidai.com>
2021-11-09 15:28:13 +0800

    init

--- docs/.vuepress/components/SiteFooter.vue
+++ docs/.vuepress/components/SiteFooter.vue
@@ -3,7 +3,7 @@
     MIT Licensed | Copyright © 2021-present Knox zhang
     <div>
       备案号：<a
-        href="http://www.beian.miit.gov.cn/"
+        href="https://beian.miit.gov.cn/"
         target="_blank"
         style="color:#4e6e8e"
         >粤ICP备2021131858号</a
